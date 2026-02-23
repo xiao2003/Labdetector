0a8298cba3dbfba09c47452f7fdd71c44c21482e
xiao2003 <895299025@qq.com>
2026-02-24 03:33:06 +0800

    V2.3.0架构更新

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="labdetector",
-    version="2.0.0",
+    version="2.3.0",
     description="智能多模态实验室管家 (RAG + 边缘计算版)",
     author="Xiao",
     packages=find_packages(),
